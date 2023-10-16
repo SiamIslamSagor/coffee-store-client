4ff87ff5b1747c6d22ef0ca7590349e16e853e43
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-17 01:56:34 +0600

    install firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRDVzh8FcWJsogyF-z5vgvpHcpIdp2MCM",
+  authDomain: "coffee-store-4b800.firebaseapp.com",
+  projectId: "coffee-store-4b800",
+  storageBucket: "coffee-store-4b800.appspot.com",
+  messagingSenderId: "428223938930",
+  appId: "1:428223938930:web:430430ccd6384b5d771deb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
