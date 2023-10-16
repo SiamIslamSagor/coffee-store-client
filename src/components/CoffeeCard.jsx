ee0b7606361262fe9568625c2876c617f68ec05c
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-16 11:28:19 +0600

    added a functionlity to delet a item

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,38 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = coffee;
-  console.log(photo);
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+  // console.log(photo);
+
+  const handleCoffeeDelete = _id => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(result => {
+      if (result.isConfirmed) {
+        // to delete data when user clicked the conform btn
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="card  card-side bg-base-100 shadow-xl">
       <figure>
@@ -21,8 +52,15 @@ const CoffeeCard = ({ coffee }) => {
         <div className="card-actions justify-end">
           <div className="btn-group btn-group-vertical space-y-2">
             <button className="btn">View</button>
-            <button className="btn">Edit</button>
-            <button className="btn">X</button>
+            <Link to={`/updateCoffee/:${_id}`}>
+              <button className="btn">Edit</button>
+            </Link>
+            <button
+              onClick={() => handleCoffeeDelete(_id)}
+              className="btn bg-red-700 text-white"
+            >
+              X
+            </button>
           </div>
         </div>
       </div>
